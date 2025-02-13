2b2d7d92b965e6494538e0dced4e34c812bd16dc
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-02-13 15:24:16 -0500

    Explicacion de expresion y componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import './App.css'
 
-function App() {
+const mensaje = 'Hola Mundo'
+
+// -----------------------------------------------
+
+// Creacion de nuevos componentes reutilizables
+
+// Creacion de nuevo componente para el mensaje
+const Mensaje = () => {
+  return <h4>{mensaje}</h4>
+}
+
+// Creacion de nuevo componente para la descripcion
+const Descripcion = () => {
+  return <p>Este es un componente reutilizable</p>
+}
+
+// -----------------------------------------------
+
+export default function App() {
+  
   return (
     <>
       <div>
-        Hola Mundo
+        {/* Se utiliza las llaves para evaluar expresiones */}
+        {mensaje}
+
+        {/* Se pueden crear componentes reutilizables */}
+        <Mensaje />
+        <Mensaje />
+        <Descripcion />
+        <Descripcion />
+
+        { /* No se pueden evaluar objetos */}
+        { /* {new Date()} */}
+        {+ new Date()}
       </div>
     </>
   )
 }
-
-export default App
